cee4d0ea95a9ba769d46f2f6fb8d7766603a3b9e
rakshith1994 <rakshithkh98@gmail.com>
2022-08-09 00:36:59 -0700

    add jump game two

---
+++ greedy/jum_game_two.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class JumpGameTwo:
+    def jump_gameTwo(self, nums: List[int]) -> int:
+        result = 0
+        left, right = 0, 0
+
+        while right < len(nums) - 1:
+            far = 0
+            for i in range(left, right + 1):
+                far = max(far, i + nums[i])
+            left = right + 1
+            right = far
+            result += 1
+        return result
