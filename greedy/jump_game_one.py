0387bd3351ecdf6c4550312a54d0f24cdc31e5b2
rakshith1994 <rakshithkh98@gmail.com>
2022-08-08 23:41:42 -0700

    add jump_game_one

---
+++ greedy/jump_game_one.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class JumpGameOne:
+    def jump_gameOne(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return True if goal == 0 else False
