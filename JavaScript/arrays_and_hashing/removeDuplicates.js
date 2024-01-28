46418a40f3e7e638b8e233d485e45343d39ebe72
rakshith1994 <rakshithkh98@gmail.com>
2024-01-28 09:52:12 -0800

    stacks baseball game sltn

--- JavaScript/arrays_and_hashing/removeDuplicates.js
+++ JavaScript/arrays_and_hashing/removeDuplicates.js
@@ -1,9 +1,13 @@
 var removeDuplicates = (nums) => {
+    // set pointers to the initial value
     [left, right] = [0, 0];
+    // move right pointer through every element
     while (right < nums.length) {
         const [leftVal, rightVal] = [nums[left], nums[right]];
+        // check if current val is equal to previous val
         const isEqual = (leftVal === rightVal);
         if  (!isEqual) {
+            // move left pointer to next index
             left++;
             nums[left] = rightVal;
         }
