965fc1b68c19ec59fef7748aea636e34b1a44fee
rakshith1994 <rakshithkh98@gmail.com>
2024-10-17 21:50:22 -0700

    search and insert

--- Python/arrays_&_hashing/plus_one.py
+++ Python/arrays_&_hashing/plus_one.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def plusOne(digits: List(int)) -> List(int):
+def plusOne(self, digits: List(int)) -> List(int):
     for i in range(len(digits) - 1, -1, -1):
         digits[i] += 1
         if digits[i] < 10:
