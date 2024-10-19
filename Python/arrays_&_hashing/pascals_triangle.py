5367021b4f699e7d1aa1514c6bef10c892c75e6d
rakshith1994 <rakshithkh98@gmail.com>
2024-10-18 21:48:11 -0700

    pascals tri sltn

---
+++ Python/arrays_&_hashing/pascals_triangle.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def pascals_triangle(self, num_rows: int) -> List[List[int]]:
+    triangle = []
+    for i in range(num_rows):
+        row = [1] * (i + 1)
+        
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
